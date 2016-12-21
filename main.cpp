175ca46926502861b21d43d5df054ad28bb704fe
Vladimir Bochkov <vlbochk@mera.ru>
2016-12-21 13:33:01 +0300

    added logs

--- main.cpp
+++ main.cpp
@@ -7,10 +7,19 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
+    std::string json_file;
+    if (argc > 1) {
+        if (strcmp(argv[1], "off") == 0)
+            json_file = "../fire_overlay_off.json";
+        else
+            json_file = "../fire_overlay_on.json";
+    } else
+        json_file = "../fire_overlay_on.json";
+
     qRegisterMetaType<CVKernel::CVProcessData>("CVProcessData");
     CVKernel::CVProcessManager *manager(new CVKernel::CVProcessManager());
     CVKernel::CVForestParser process = CVKernel::CVForestParser()
-            .parseJSON("../fire.json");
+            .parseJSON(json_file.c_str());
     manager->addNewStream(process);
     for (CVKernel::CVProcessTree* tree : process.get_forest()) delete tree;
     process.get_forest().clear();
